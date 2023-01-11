4058361c9435167673e3bddeadc393276129f3c8
Gabriel <gabriel.ogawa@hotmail.com>
2023-01-11 18:05:27 -0300

    feat: add login route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import "reflect-metadata"
-import express from "express"
-import "express-async-errors"
+import "reflect-metadata";
+import express from "express";
+import "express-async-errors";
+import sessionRoutes from "./routes/session.routes";
 
+const app = express();
+app.use(express.json());
+app.use("/login", sessionRoutes);
 
-
-const app = express()
-app.use(express.json())
-
-
-export default app
+export default app;
---
+++ src/services/createSession.service.ts
@@ -0,0 +1,45 @@
+import { compare } from "bcryptjs";
+import AppDataSource from "../data-source";
+import { User } from "../entities/user.entity";
+import { AppError } from "../errors/errors";
+import { IUserLogin } from "../interfaces/login/index";
+import jwt from "jsonwebtoken";
+
+const createSessionService = async ({
+  email,
+  password,
+}: IUserLogin): Promise<string> => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const user = await userRepository.findOneBy({
+    email: email,
+  });
+
+  if (!user) {
+    throw new AppError("Email or password invalid", 403);
+  }
+
+  if (!user.isActive) {
+    throw new AppError("User is inactive", 403);
+  }
+
+  //   const passwordVerify = await compare(password, user.password);
+  //   if (!passwordVerify) {
+  //     throw new AppError("Email or password invalid", 403);
+  //   }
+
+  const token = jwt.sign(
+    {
+      type: user.type,
+    },
+    process.env.SECRET_KEY!,
+    {
+      subject: String(user.id),
+      expiresIn: "24h",
+    }
+  );
+
+  return token;
+};
+
+export default createSessionService;
