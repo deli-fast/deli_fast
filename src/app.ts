02cf858f869e3cc9fedd44305eca9d7e49de1b39
Daniel Lobato <daniel.lobato2001@gmail.com>
2023-01-13 11:14:21 -0300

    feat: ends products routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import "reflect-metadata"
-import express from "express"
-import "express-async-errors"
+import "reflect-metadata";
+import express from "express";
+import "express-async-errors";
+import productRouter from "./routes/products.routes";
 
+const app = express();
+app.use(express.json());
 
+app.use("/products", productRouter);
 
-const app = express()
-app.use(express.json())
-
-
-export default app
+export default app;
---
+++ src/services/updateProduct.service.ts
@@ -0,0 +1,16 @@
+import AppDataSource from "../data-source";
+import { Product } from "../entities/product.entity";
+
+const updateProductService = async (id, { name, stock, typeId }) => {
+  const product = await AppDataSource.createQueryBuilder()
+    .from(Product, "products")
+    .update()
+    .set({ name, stock })
+    .where("id = :id_product", { id_product: id })
+    .returning("*")
+    .execute();
+
+  return product.raw[0];
+};
+
+export default updateProductService;
