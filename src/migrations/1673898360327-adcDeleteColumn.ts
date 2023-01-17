1a7c5038ff534649fb58b284a574426a8d9552a7
Marcela <mamadipaula@gmail.com>
2023-01-17 09:10:47 -0300

    fix: orderEntity

--- src/migrations/1673898360327-adcDeleteColumn.ts
+++ src/migrations/1673898360327-adcDeleteColumn.ts
@@ -4,7 +4,6 @@ export class adcDeleteColumn1673898360327 implements MigrationInterface {
     name = 'adcDeleteColumn1673898360327'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" ADD "deleteAt" TIMESTAMP`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "telephone"`);
         await queryRunner.query(`ALTER TABLE "users" ADD "telephone" character varying NOT NULL`);
     }
